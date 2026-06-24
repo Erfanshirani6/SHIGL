8b2653852a890890cc78265202a07ccf5343e159
Erfanshirani6 <97264444+Erfanshirani6@users.noreply.github.com>
2026-06-24 21:20:03 +0330

    Update shigl.py

--- shigl.py
+++ shigl.py
@@ -55,3 +55,104 @@ while True:
 
     else:
         print("دستور ناشناخته")
+___
+print("SHIGL v0.3 Started")
+
+variables = {}
+
+while True:
+    code = input("SHIGL> ")
+
+    if code == "exit":
+        break
+
+    # متغیر
+    elif code.startswith("var "):
+        try:
+            left, right = code[4:].split("=")
+            name = left.strip()
+            value = right.strip().replace('"', '')
+
+            if value.isdigit():
+                value = int(value)
+
+            variables[name] = value
+
+        except:
+            print("خطا در متغیر")
+
+    # چاپ
+    elif code.startswith("say "):
+        text = code[4:].strip()
+
+        if text in variables:
+            print(variables[text])
+        else:
+            print(text.replace('"', ''))
+
+    # شرط
+    elif code.startswith("if "):
+
+        condition = code[3:]
+
+        try:
+
+            if ">" in condition:
+
+                left, right = condition.split(">")
+
+                left = left.strip()
+                right = right.strip()
+
+                if left in variables:
+                    left = variables[left]
+
+                if right in variables:
+                    right = variables[right]
+
+                if int(left) > int(right):
+                    print("TRUE")
+                else:
+                    print("FALSE")
+
+            elif "<" in condition:
+
+                left, right = condition.split("<")
+
+                left = left.strip()
+                right = right.strip()
+
+                if left in variables:
+                    left = variables[left]
+
+                if right in variables:
+                    right = variables[right]
+
+                if int(left) < int(right):
+                    print("TRUE")
+                else:
+                    print("FALSE")
+
+            elif "==" in condition:
+
+                left, right = condition.split("==")
+
+                left = left.strip()
+                right = right.strip()
+
+                if left in variables:
+                    left = variables[left]
+
+                if right in variables:
+                    right = variables[right]
+
+                if str(left) == str(right):
+                    print("TRUE")
+                else:
+                    print("FALSE")
+
+        except:
+            print("خطا در شرط")
+
+    else:
+        print("دستور ناشناخته")
