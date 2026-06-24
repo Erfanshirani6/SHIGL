04e37111c3967dd6b6b236af674af4a49a8c1b74
Erfanshirani6 <97264444+Erfanshirani6@users.noreply.github.com>
2026-06-24 16:54:33 +0330

    Update interpreter.py

--- interpreter.py
+++ interpreter.py
@@ -1,1 +1,24 @@
+# SHIGL Interpreter v0.1
 
+print("SHIGL Language Started")
+
+while True:
+    code = input("SHIGL> ")
+
+    if code == "exit":
+        break
+
+    elif code.startswith("say "):
+        print(code[4:].replace('"', ''))
+
+    elif code.startswith("ask "):
+        var = code[4:]
+        value = input(var + ": ")
+        print(value)
+
+    elif code == "time":
+        from datetime import datetime
+        print(datetime.now().strftime("%H:%M:%S"))
+
+    else:
+        print("Unknown command")
