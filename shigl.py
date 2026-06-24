b94f22fbfa67bc4adba85059c0d32f90184d761a
Erfanshirani6 <97264444+Erfanshirani6@users.noreply.github.com>
2026-06-24 21:10:54 +0330

    Update shigl.py

--- shigl.py
+++ shigl.py
@@ -11,3 +11,17 @@ while True:
 
     else:
         print("دستور ناشناخته")
+___
+print("SHIGL Started")
+
+while True:
+    code = input("SHIGL> ")
+
+    if code == "exit":
+        break
+
+    elif code.startswith("say "):
+        print(code[4:].replace('"', ''))
+
+    else:
+        print("دستور ناشناخته")
