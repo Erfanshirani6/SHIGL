614275ea3920bf458588af6ad54a83c5f74a2c36
Erfanshirani6 <97264444+Erfanshirani6@users.noreply.github.com>
2026-06-24 20:48:33 +0330

    Create shigl.py

---
+++ shigl.py
@@ -0,0 +1,13 @@
+print("SHIGL Started")
+
+while True:
+    code = input("SHIGL> ")
+
+    if code == "exit":
+        break
+
+    elif code.startswith("say "):
+        print(code[4:].replace('"', ''))
+
+    else:
+        print("دستور ناشناخته")
