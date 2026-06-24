e45ac590458f1c7d56e6ccefd0cc726dfe278d91
Erfanshirani6 <97264444+Erfanshirani6@users.noreply.github.com>
2026-06-24 21:13:10 +0330

    Update shigl.py

--- shigl.py
+++ shigl.py
@@ -25,3 +25,33 @@ while True:
 
     else:
         print("دستور ناشناخته")
+___
+print("SHIGL v0.2 Started")
+
+variables = {}
+
+while True:
+    code = input("SHIGL> ")
+
+    if code == "exit":
+        break
+
+    elif code.startswith("var "):
+        try:
+            left, right = code[4:].split("=")
+            name = left.strip()
+            value = right.strip().replace('"', '')
+            variables[name] = value
+        except:
+            print("خطا در متغیر")
+
+    elif code.startswith("say "):
+        text = code[4:].strip()
+
+        if text in variables:
+            print(variables[text])
+        else:
+            print(text.replace('"', ''))
+
+    else:
+        print("دستور ناشناخته")
